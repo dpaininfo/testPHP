4105987aab29cc9d47e920e9f005aabac123aaf6
Delphine Pain <delphine.pain@yahoo.fr>
2023-10-20 17:08:11 +0200

    ajout de traitement.php

---
+++ traitement.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <title>Premier formulaire</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+    <h1>Bonjour ! C'est mon 1er PHP</h1>
+
+
+<?php
+
+
+$nom=$_REQUEST["nom"];
+
+echo $nom." est sympa";
+
+?>
+
+</body>
+</html>
